f2a59d87f3b013713b38572c18b62edfd7289aff
CuminBeef <2523097018@qq.com>
2019-09-03 23:43:34 +0800

    schedule

---
+++ src/main/java/com/adonis/springbootquartz/schedule/ScheduledDemo.java
@@ -0,0 +1,14 @@
+package com.adonis.springbootquartz.schedule;
+
+import java.util.Date;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledDemo {
+	@Scheduled(cron = "0/2 * * * * ?")
+	public void scheduledMethod() {
+		System.out.println("定时器被触发" + new Date());
+	}
+}
